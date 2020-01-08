9f7d60906dc36e11699401c825e5a29ffaad177e
unknown <alxmer111@gmail.com>
2020-01-08 14:30:10 +0400

    handlebars

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express');
-const app     = express();
+const path    = require('path');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
 
 const PORT = process.env.PORT || 3000;
 
