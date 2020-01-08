6128fb4353a101c805db55d93aaca02adb614b62
unknown <alxmer111@gmail.com>
2020-01-08 16:22:11 +0400

    navigation menu

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const path    = require('path');
+const path = require('path');
+const fs = require('fs');
 const exphbs  = require('express-handlebars');
 
 const app = express();
@@ -13,12 +14,27 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Главная страница',
+    isHome: true
+  });
 })
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get('/games', (req, res) => {
+  res.render('games', {
+    title: 'Каталог игр',
+    isGames: true
+  });
+})
+
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: 'Добавить игру',
+    isAdd: true
+  });
 })
 
 const PORT = process.env.PORT || 3000;
